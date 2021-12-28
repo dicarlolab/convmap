573a9223be209681ee0814161bdb6c9879967164
Pouya Bashivan <poya.bashivan@gmail.com>
2021-12-27 20:11:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=['convmap'],
     install_requires=['numpy', 'scipy', 'h5py', 'tensorflow-gpu'],
-    url='https://github.com/dicarlolab/cube_mapping.git',
+    url='https://github.com/dicarlolab/convmap.git',
     license='GNU GENERAL PUBLIC LICENSE',
     author='Pouya Bashivan',
     description="Neural mapping from convolutional feature maps."
